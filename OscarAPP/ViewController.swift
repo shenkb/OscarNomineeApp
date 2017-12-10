5d87d1b5789d8892d83cd9e0f7d2104655d25734
傻蹦蹦 <kaining@kainings-mbp.bc.edu>
2017-12-10 00:03:16 -0500

    Oscar Simple App Upload!

--- OscarAPP/ViewController.swift
+++ OscarAPP/ViewController.swift
@@ -9,17 +9,50 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var resultsTextView: UITextView!
+    
+    let bestPicture = ["Arrival",
+                       "Fences",
+                       "Hacksaw Ridge",
+                       "Hell or High Water",
+                       "Hidden Figures",
+                       "La La Land",
+                       "Lion",
+                       "Manchester by the Sea",
+                       "Moonlight"]
+    let bestActress = ["Isabelle Huppert - Elle",
+                       "Ruth Negga - Loving",
+                       "Natalie Portman - Jackie",
+                       "Emma Stone - La La Land",
+                       "Meryl Streep - Florence Foster Jenkins"]
+    let bestActor = ["Casey Affleck - Manchester by the Sea", "Andrew Garfield - Hacksaw Ridge", "Ryan Gosling - La La Land", "Viggo Mortensen - Captain Fantastic", "Denzel Washington - Fences"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        resultsTextView.text = arrayToString(array: bestPicture)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func arrayToString(array: [String]) -> String {
+        var newArray = ""
+        for index in 0...array.count - 1 {
+            newArray = newArray + array[index] + "\n"
+        }
+        return newArray
     }
-
-
+    
+    @IBAction func segmentedPressed(_ sender: UISegmentedControl) {
+        switch segmentedControl.selectedSegmentIndex {
+        case 0:
+            resultsTextView.text = arrayToString(array: bestPicture)
+        case 1:
+            resultsTextView.text = arrayToString(array: bestActress)
+        case 2:
+            resultsTextView.text = arrayToString(array: bestActor)
+        default:
+            resultsTextView.text = "Could not appear the right solution. Check again!"
+        }
+    }
+    
 }
 
